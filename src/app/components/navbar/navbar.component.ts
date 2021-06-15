b136ad98fa2842e0e224d46ad75beb3280ade7b1
David <leondav@hotmail.com>
2021-06-15 20:20:18 -0300

    navbar con pagina actual

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-navbar',
@@ -13,15 +15,47 @@ export class NavbarComponent implements OnInit {
   formacion = false;
   experiencia = false;
   contacto = false;
+  url="";
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private _location: Location) {
+    this.url = this.router.routerState.snapshot.url;
+    console.log("url",this.url);
+    //tambien puedo definir la posicion actual usando esta variable
+
+  }
 
   ngOnInit(): void {
     console.log(this.actual);
-    console.log(this.router);
-    switch(this.actual) {
+    switch (this.actual) {
       case 1:
         this.home = true;
+        this.formacion = false;
+        this.experiencia = false;
+        this.contacto = false;
+        break;
+      case 2:
+        this.home = false;
+        this.formacion = true;
+        this.experiencia = false;
+        this.contacto = false;
+        break;
+      case 3:
+        this.home = false;
+        this.formacion = false;
+        this.experiencia = true;
+        this.contacto = false;
+        break;
+      case 4:
+        this.home = false;
+        this.formacion = false;
+        this.experiencia = false;
+        this.contacto = true;
+        break;
+      default:
+        this.home = false;
+        this.formacion = false;
+        this.experiencia = false;
+        this.contacto = false;
         break;
     }
   }
