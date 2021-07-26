a1cc1b8699c18659e33d1bd2b0f10343c1435b07
David <leondav@hotmail.com>
2021-07-25 21:58:01 -0300

    build

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Experiencia } from '../../api/interfaces';
 
 const EXPERIENCIA: Experiencia[] =[
-  {empresa: "OneInfo Consulting",inicio:"Marzo 2021",fin:"",puesto:"Desarrollador web fullstack",descripcion:"Desarrollo de frontend tanto en React como en Angular. Trato con clientes en reuniones y demostraciones. Ayudé a otros colaboradores de la empresa que requerían asistencia en proyectos ajenos a mi. OneInfo Consulting es mi primer trabajo como desarrollador.",logo:"oneinfoconsulting.PNG"},
+  {empresa: "OneInfo Consulting",inicio:"Marzo 2021",fin:"",puesto:"Desarrollador web fullstack",descripcion:"Desarrollo de frontend tanto en React como en Angular. Trato con clientes en reuniones y demostraciones. Ayudé a otros colaboradores de la empresa que requerían asistencia en proyectos ajenos a mi. OneInfo Consulting es mi primer trabajo como desarrollador.",logo:"oneinfoconsulting.png"},
 ];
 
 @Component({
