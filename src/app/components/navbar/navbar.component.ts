69022bcb5326ccfb08f261425c3213c61c715098
David <leondav@hotmail.com>
2021-08-20 22:21:35 -0300

    theme changer service

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from "@angular/router";
-import { Location } from '@angular/common';
 import { MobileDetectorService } from '../../services/mobileDetector/mobile-detector.service';
-
+import { ThemeChangerService } from '../../services/themeChanger/theme-changer.service';
 
 @Component({
   selector: 'app-navbar',
@@ -16,29 +14,16 @@ export class NavbarComponent implements OnInit {
   formacion = false;
   experiencia = false;
   contacto = false;
-  url = "";
   isMobile: boolean = false;
   isDark = true;
 
-  constructor(private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private _location: Location,
-    private mobileDetector: MobileDetectorService,) {
-    this.url = this.router.routerState.snapshot.url;
-    console.log("url", this.url);
-    //tambien puedo definir la posicion actual usando esta variable
-
+  constructor(
+    private mobileDetector: MobileDetectorService,
+    private themeChanger: ThemeChangerService) {
   }
 
   ngOnInit(): void {
-    document.documentElement.style.setProperty('--bodyBackground', 'darkblue');
-    document.documentElement.style.setProperty('--textColor', 'white');
-    document.documentElement.style.setProperty('--subtitleColor', 'chartreuse');
-    document.documentElement.style.setProperty('--formacionColor', '#424242');
-    document.documentElement.style.setProperty('--title', 'rgb(228, 210, 139)');
-    document.documentElement.style.setProperty('--shadowColor', 'white');
-    document.documentElement.style.setProperty('--iconColor', 'darkblue');
-    document.documentElement.style.setProperty('--modeColor', 'yellow');
+    this.themeChanger.darkTheme();
     this.isMobile = this.mobileDetector.isMobile();
     switch (this.actual) {
       case 1:
@@ -75,26 +60,7 @@ export class NavbarComponent implements OnInit {
   }
 
   changeTheme() {
-    if(this.isDark) {
-      document.documentElement.style.setProperty('--bodyBackground', 'whitesmoke');
-      document.documentElement.style.setProperty('--textColor', 'black');
-      document.documentElement.style.setProperty('--subtitleColor', 'darkblue');
-      document.documentElement.style.setProperty('--title', 'red');
-      document.documentElement.style.setProperty('--formacionColor', '#7c96d9');
-      document.documentElement.style.setProperty('--shadowColor', 'red');
-      document.documentElement.style.setProperty('--iconColor', 'black');
-      document.documentElement.style.setProperty('--modeColor', 'white');
-    } else {
-      document.documentElement.style.setProperty('--bodyBackground', 'darkblue');
-      document.documentElement.style.setProperty('--textColor', 'white');
-      document.documentElement.style.setProperty('--subtitleColor', 'chartreuse');
-      document.documentElement.style.setProperty('--title', 'rgb(228, 210, 139)');
-      document.documentElement.style.setProperty('--formacionColor', '#424242');
-      document.documentElement.style.setProperty('--shadowColor', 'white');
-      document.documentElement.style.setProperty('--iconColor', 'darkblue');
-      document.documentElement.style.setProperty('--modeColor', 'yellow');
-    }
-    this.isDark = !this.isDark
+    this.isDark = this.themeChanger.changeTheme();
   }
 
 }
